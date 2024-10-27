a3b21fa0bd8bf28d45a3afee04d8870ad730db2b
Bikash Kakati <bikashkakati796@gmail.com>
2024-10-27 15:25:19 +0530

    Add: Added dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { mongodbUrl, serverPort } from "./config";
 import connectDb from "./db";
-import { app } from "./models/app";
+import { app } from "./app";
 
-const port = serverPort;
+serverPort;
 
 connectDb(mongodbUrl!);
 
-app.listen(port, () => {
-  console.log(`server started on ${port}`);
+app.listen(serverPort, () => {
+  console.log(`server started on ${serverPort||6000}`);
 });
