68696e92cab14ba6ebbc94feaf070c36c0542732
Bikash Kakati <bikashkakati796@gmail.com>
2024-11-01 22:34:45 +0530

    Add: Role & status functionality

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,113 @@
+import bcrypt from "bcryptjs";
+import { Request, Response } from "express";
+import { ObjectId } from "mongoose";
+import { tokenName } from "../config";
+import { cookieOptions } from "../constants";
+import { RegisteredUsers } from "../models/register.model";
+import { generateTokens, isInviteExpired, validateRequest } from "../utils";
+import {
+  userLoginSchemaZod,
+  UserLoginSchemaZodType,
+  userRegisterSchemaZod,
+  UserRegisterSchemaZodType
+} from "../validation/userAuthSchemaZod";
+import { DoctorInvite } from "../models/doctor/doctorInvite.model";
+
+export const handleRegister = async (
+  req: Request<{}, {}, UserRegisterSchemaZodType>,
+  res: Response
+): Promise<void> => {
+  try {
+    const { success, data } = await validateRequest(
+      userRegisterSchemaZod,
+      req.body
+    );
+    if (!success) {
+      return res.customResponse(400, "fields are not valid", data);
+    }
+
+    const { name, email, password, secretKey} = data;
+    
+    // handling doctor invite
+    // const doctorInviteDetails = await DoctorInvite.findOne({email});
+
+    function getRole(){
+      if(secretKey === "68"){
+        return "admin";
+      }
+      // if(doctorInviteDetails?.status === "pending"){
+      //   return "doctor";
+      // }
+        return "doctor";
+    }
+
+    const role = getRole();
+
+    // if(role === "doctor"){
+    //   if(isInviteExpired(doctorInviteDetails?.createdAt,2)){
+    //     doctorInviteDetails?.set("status","expired");
+    //     await doctorInviteDetails?.save();
+    //     return res.customResponse(400,"Invitation has expired");
+    //   }
+    //   doctorInviteDetails?.set("status","accepted");
+    //   await doctorInviteDetails?.save();
+    // }
+
+    const registeredUser = new RegisteredUsers({
+      name,
+      email,
+      password,
+      role
+    });
+    registeredUser.password = "";
+
+
+    await registeredUser.save();
+
+    res.customResponse(200, "User Successfully Registered", registeredUser);
+  } catch (error: any) {
+    if (error.code === 11000) {
+      res.customResponse(400, "Email already exists");
+    } else {
+      res.customResponse(500, "Error on registering user", error);
+    }
+  }
+};
+
+export const handleLogin = async (
+  req:  Request<{}, {}, UserLoginSchemaZodType>,
+  res: Response
+): Promise<void> => {
+  try{
+    const { success, data } = await validateRequest(
+      userLoginSchemaZod,
+      req.body
+    );
+    if (!success) {
+      return res.customResponse(400, "fields are not valid", data);
+    }
+    const { email, password } = data;
+  
+    const user = await RegisteredUsers.findOne({ email }).select("+password");
+  
+    if (!user) {
+      return res.customResponse(404, "User not found/email incorrect");
+    }
+  
+    const passwordMatch = await bcrypt.compare(password, user.password);
+  
+    if (!passwordMatch) {
+      res.customResponse(400, "Password does not match");
+    }
+    
+    const token = generateTokens(user._id as ObjectId, user.role);
+  
+    user.password = ""; // because it gives type error when assign undefined
+  
+    return res
+      .cookie(tokenName, token, cookieOptions)
+      .customResponse(200, "Login successfully", user);
+  }catch(err){
+    res.customResponse(500,"Error on Loging user");
+  }
+};
