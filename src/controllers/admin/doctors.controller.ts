8498b043554a22b1ef692e323c4084f37aa4a949
Bikash Kakati <bikashkakati796@gmail.com>
2024-11-01 22:38:05 +0530

    Add: Get doctor apis

---
+++ src/controllers/admin/doctors.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { RegisteredUsers } from "../../models/register.model";
+
+export const handleGetAllDoctors = async (req: Request, res: Response) => {
+  try {
+    const allDoctors = await RegisteredUsers.find({ role: "doctor" });
+    res.customResponse(200, "All doctors are here", allDoctors);
+  } catch (err: any) {
+    res.customResponse(400, `Failed to fetch doctors:${err.message}`);
+  }
+};
+
+export const handleGetDoctorById = async (req: Request, res: Response) => {
+  try {
+    const doctorInfo = await RegisteredUsers.findById(req.params.id);
+    res.customResponse(
+      200,
+      `Fetched details of ${doctorInfo?.name}`,
+      doctorInfo!
+    );
+  } catch (err: any) {
+    res.customResponse(400, `Failded to fetch details`);
+  }
+};
